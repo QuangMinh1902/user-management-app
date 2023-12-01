82d897d48d34f6b06df2d6b2f1a9ee6b504ee93a
Quang Minh PHAM <quangminh1575@gmail.com>
2023-12-01 09:18:48 +0100

    fea: main page

---
+++ src/crud app/Home.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3030/users")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="container mt-5">
+    <Link to="/create" className="btn btn-success my-3">Create User</Link>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((d, i) => (
+            <tr key={i}>
+              <td>{d.id}</td>
+              <td>{d.name}</td>
+              <td>{d.email}</td>
+              <td>
+                <button>Update</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
